a32f1c589e40e91925ce083a03202d15775120b0
Ananan29 <arora.ananya404@gmail.com>
2024-08-02 00:02:28 +0530

    Update Navigationbar.jsx

--- src/components/Navigationbar/Navigationbar.jsx
+++ src/components/Navigationbar/Navigationbar.jsx
@@ -31,3 +31,37 @@ const Navigationbar = () => {
 }
 
 export default Navigationbar
+
+// import React,{useState} from "react";
+// import "./Navigationbar.css";
+// import {IoIosBody} from "react-icons/io";
+// import logo from "../../assets/purplestar.png"
+// import Authopopup from "../Authopopup/Authopopup";
+
+
+
+// const Navigationbar = () => {
+//   const [isloggedin,setisloggedin]=useState(false)
+//   const [showpopup, setShowpopup] = useState(false)
+//   return (
+//     <nav>
+//       <img src={logo} alt="logo" className="img"/>
+//       <a href="/" className="a">Home</a>
+//       <a href="/about" className="a">About</a>
+//       <a href="/profile" className="a"><IoIosBody/></a>
+//       {
+//         isloggedin?
+//         <button className="btn">Logout</button>:
+//         <button onClick={() => {
+//           setShowpopup(true)
+//         }}>Login</button>
+//       }
+//       {
+//         showpopup&& <Authopopup setShowpopup={setShowpopup}/>
+//         // <h1 className="mainhead1">Login page</h1>
+//       }
+//     </nav>
+//   )
+// }
+
+// export default Navigationbar
